9bb134643bda4a25bf4be07249b0b259d07cb8fa
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-05-01 18:03:51 +0500

    Add files via upload

---
+++ wordcounter.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+let condition =true
+while(condition){
+const result = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : "input",
+        message : "enter your sentence/word which you want to count "
+    }
+])
+
+let result1 = result.sentence.trim().split(" ")
+console.log(result1)
+
+ let words = result1.length
+  console.log("your sentence word count is " + words)
+}
